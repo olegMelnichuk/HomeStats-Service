41ab12117a12ccf71686a7964b63f22c54c1473d
olegMelnichuk <oleg.melnichuk@gmail.com>
2018-08-21 12:28:04 +0300

    fix for secret reading

--- controllers/deploy.js
+++ controllers/deploy.js
@@ -12,7 +12,8 @@ var DeploymentLogs = require('../models/deploymentLogs');
 exports.check = function(req, res, next) {
     var serviceName = req.params.serviceName;
     var githubData = config.get('gitAutomation');
-    var serverKey = 'sha1=' + crypto.createHmac('sha1', githubData[serviceName].githubSecret).update(JSON.stringify(req.body)).digest('hex');
+    var payload = JSON.stringify(req.body);
+    var serverKey = 'sha1=' + crypto.createHmac('sha1', githubData[serviceName].githubSecret).update(payload).digest('hex');
     var githubKey = req.headers['x-hub-signature'] || '';
     if (bufferEq(new Buffer(githubKey), new Buffer(serverKey))) {
         next();
@@ -25,20 +26,24 @@ exports.make = function(req, res, next) {
     var projectPath = config.get('projectPath');
     var gitAutomation = config.get('gitAutomation');
     var serviceName = req.params.serviceName;
-    if (shell.exec('bash ' + projectPath + 'deploy.sh ' + serviceName + ' ' + gitAutomation[serviceName].projectPath + ' > ' + projectPath + 'deployLog.log').code == 0) {
-        var payload = req.body.payload;
-        var data = new DeploymentLogs({
-            project: serviceName,
-            branch: payload.repository.default_branch,
-            status: true,
-            commit_id: payload.commits[0].id,
-            payload: payload,
-            created_at: new Date().getTime(),
-            updated_at: new Date().getTime()
-        });
-        res.data = data;
-        console.log('Updates were deployed successfully');
-        next();
+    var payload = req.body.payload;
+    if (payload && payload.length != 0) {
+        if (shell.exec('bash ' + projectPath + 'deploy.sh ' + serviceName + ' ' + gitAutomation[serviceName].projectPath + ' > ' + projectPath + 'deployLog.log').code == 0) {
+            var data = new DeploymentLogs({
+                project: serviceName,
+                branch: payload.repository.default_branch,
+                status: true,
+                commit_id: payload.commits[0].id,
+                payload: payload,
+                created_at: new Date().getTime(),
+                updated_at: new Date().getTime()
+            });
+            res.data = data;
+            console.log('Updates were deployed successfully');
+            next();
+        } else {
+            res.status(200).send({ message: 'Testing is ok' });
+        }
     } else {
         res.status(500).send({ message: 'Deployment bash script got failed please check the deployLog.log file' });
     }
@@ -51,9 +56,3 @@ exports.save = function(req, res) {
     });
     res.status(200).send({ message: '200 OK The updates were deployed successfully' });
 };
-
-/*
- exports.status = function(req, res) {
-     res.status(200).send({ message: '200 OK (Get request has been done)' });
- }; 
-*/
